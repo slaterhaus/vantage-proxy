acde9e2381f97fc4d023d5653b52e5c740a5483a
James Slater <thomas.james.slater@gmail.com>
2024-02-25 12:58:54 -0500

    adding OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { StocksModule } from '../stocks/stocks.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [StocksModule],
+  imports: [StocksModule, ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'public'), // Serve static files from the 'public' directory
+  }),],
   controllers: [],
   providers: []
 })
